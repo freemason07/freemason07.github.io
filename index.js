9adfb06d291922c3af6b2af073ffeba19a869a5e
freemason07 <129541588+freemason07@users.noreply.github.com>
2023-04-01 01:27:18 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const NikeE1=document.getElementById
+("Nike")
+const AmazonE1=document.getElementById
+("Amazon")
+const SheinE1=document.getElementById
+("Shein")
+const AppleE1=document.getElementById
+("Apple")
+function(){
+    let h=new Date().getHours()
+    let m=new Date().getMinutes()
+    let s=new Date().getSeconds()
+}
